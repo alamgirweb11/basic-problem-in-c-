da606670a663821a91bad6e98675e720c560f187
alamgirweb11 <alamgirweb11@gmail.com>
2022-01-05 19:50:00 +0600

    series problem set-1

---
+++ series/3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+// problem : 1^1+2^2+3^3+.....+n^n
+ int num, sum = 0, i;
+
+ cout << "Enter any number : ";
+
+ cin >> num;
+
+ for(i = 1; i <= num; i++){
+
+    sum = sum + i^i;
+
+ }
+    cout << "Total some of series = " << sum;
+
+return 0;
+}
